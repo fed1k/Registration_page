d2a037b2e635f427511d7cace5d8b5664258644a
fed1k <93676386+fed1k@users.noreply.github.com>
2021-12-03 21:14:03 +0500

    Add files via upload

---
+++ log.js
@@ -0,0 +1,10 @@
+const login=document.querySelector(".bosish")
+const login2=document.querySelector(".bosish2")
+
+login.addEventListener("click", e => {
+    document.location.href="login2.html";
+    
+})
+login2.addEventListener("click", e => {
+    document.location.href="Login.html";
+})
